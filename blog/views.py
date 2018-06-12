81cb8b506c01919bc08dbae60047ffa1e03964f4
zn12078 <287861929@qq.com>
2018-06-12 14:33:19 +0800

    five commit

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 from django.http import HttpResponse, JsonResponse
@@ -11,3 +11,6 @@ def post_list(request):
     return render(request,'blog/post_list.html',{'posts':posts})
     # return HttpResponse('ok')
 
+def post_detail(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    return render(request,'blog/post_detail.html',{'post': post})
