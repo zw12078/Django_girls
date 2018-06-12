7361229dfe46b4785a4c7ea8d17970a571f26222
zn12078 <287861929@qq.com>
2018-06-12 13:51:26 +0800

    four commit

--- blog/views.py
+++ blog/views.py
@@ -10,3 +10,4 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request,'blog/post_list.html',{'posts':posts})
     # return HttpResponse('ok')
+
